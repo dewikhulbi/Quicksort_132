ce5387ef619bfe18abcc97c271e3da38d2d19f3b
dewikhulbi <dewikhlb20@gmail.com>
2023-03-30 15:12:41 +0700

    array untuk menympan bilangan bulat

--- Quicksort_132/Quicksort_132.cpp
+++ Quicksort_132/Quicksort_132.cpp
@@ -2,29 +2,4 @@
 using namespace std;
 
 
-int arr[20]					//array of integers tp hold velues
-int cmp_count = 0;			//number of comparasion
-int mov_count = 0;			//number of data movement
-int n;
-
-void inout() {
-	while (true)
-	{
-		cout << "Masukan panjang element array: ";
-		cin >> n;
-
-		if (n <= 20)
-			break;
-		else
-			cout << "\nMaksimum panjang array adalah 20" << endl;
-	}
-
-	cout << "\n-------------------" << endl;
-	cout << "\nEnter Array Element" << endl;
-	cout << "\n-------------------" << endl;
-
-	for (int i = 0; i < n; i++)
-	{
-		cout << "<"
-	}
-}
+int arr[20]					//array of integers to hold velues
